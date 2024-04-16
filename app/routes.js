1daa254ea31d78839f3a54ff4ce5673e605a1701
Dean Cobb <51478242+nhsbsa-dean@users.noreply.github.com>
2024-04-16 13:39:09 +0100

    prototype versioning

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,8 @@ const express = require('express');
 const router = express.Router();
 
 // ePACT2 Versions
+router.use('/v3', require('./views/ePACT/v3/_routes'));
+router.use('/v2', require('./views/ePACT/v2/_routes'));
 router.use('/v1', require('./views/ePACT/v1/_routes'));
 
 // eDEN Versions

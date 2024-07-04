a7824341281cde43854ed5e167d1543f300f5c48
Dean Cobb <51478242+nhsbsa-dean@users.noreply.github.com>
2024-07-04 14:35:13 +0100

    eDEN and ePACT2 Version 4 setup

--- app/routes.js
+++ app/routes.js
@@ -4,11 +4,13 @@ const express = require('express');
 const router = express.Router();
 
 // ePACT2 Versions
-router.use('/v3', require('./views/ePACT/v3/_routes'));
-router.use('/v2', require('./views/ePACT/v2/_routes'));
-router.use('/v1', require('./views/ePACT/v1/_routes'));
+router.use('/v4', require('./views/ePACT/v4/_routes'));
+// router.use('/v3', require('./views/ePACT/v3/_routes'));
+// router.use('/v2', require('./views/ePACT/v2/_routes'));
+// router.use('/v1', require('./views/ePACT/v1/_routes'));
 
 // eDEN Versions
+router.use('/v4', require('./views/eDEN/v4/_routes'));
 // router.use('/v1', require('./views/eDEN/v1/_routes'));
 
 // eOPS Versions
